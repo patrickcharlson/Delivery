77f00ac1fdcee73ce11233b863fc2dee713a2fb8
charlson <patrickcharlson@gmail.com>
2019-02-04 10:25:13 +0300

    Restructured the application

--- app.py
+++ app.py
@@ -1,29 +1,9 @@
-from flask import Flask, render_template
-from flask_bootstrap import Bootstrap
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, EqualTo, Length
+from flask_script import Manager
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'dev'
+from app import create_app
 
-bootstrap = Bootstrap(app)
-
-
-class DataForm(FlaskForm):
-    username = StringField('Enter your name', validators=[DataRequired, Length(min=4, max=25)])
-    email = StringField('Email Address', validators=[DataRequired])
-    password = PasswordField('Password', validators=[DataRequired, EqualTo('confirm', message='Passwords must match')])
-    confirm = PasswordField('Confirm Password')
-    Submit = SubmitField('Login')
-
-
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    form = DataForm()
-
-    return render_template('index.html', form=form)
-
+app = create_app()
+manager = Manager(app)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    manager.run()
