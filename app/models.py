eb51537a748e0ebc108c4e76eebe46d91c512981
charlson <patrickcharlson@gmail.com>
2019-03-28 18:09:37 +0300

    Generating confirmation tokens

--- app/models.py
+++ app/models.py
@@ -1,4 +1,6 @@
+from flask import current_app
 from flask_login import UserMixin
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from werkzeug.security import generate_password_hash, check_password_hash
 
 from . import db
@@ -30,6 +32,23 @@ class Customer(UserMixin, db.Model):
     username = db.Column(db.String(64), unique=True, index=True)
     orders = db.relationship('Association', back_populates='customer', lazy='dynamic')
     password_hash = db.Column(db.String(128))
+    confirmed = db.Column(db.Boolean, default=False)
+
+    def generate_confirmation_token(self, expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+        return s.dumps({'confirm': self.id})
+
+    def confirm(self, token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except:
+            return False
+        if data.get('confirm') != self.id:
+            return False
+        self.confirmed = True
+        db.session.add(self)
+        return True
 
     @property
     def password(self):
