c797685ebd4374aa088b6da4fe444df19bf0f3ef
charlson <patrickcharlson@gmail.com>
2019-03-28 16:44:44 +0300

    Added a blueprint

--- app/models.py
+++ app/models.py
@@ -1,5 +1,8 @@
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
 from . import db
+from . import login_manager
 
 
 class Association(db.Model):
@@ -18,9 +21,28 @@ class Order(db.Model):
     customers = db.relationship('Association', back_populates='order', lazy='dynamic')
 
 
-class Customer(db.Model):
+class Customer(UserMixin, db.Model):
     __tablename__ = 'customers'
     id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(64), unique=True, index=True)
+    first_name = db.Column(db.String(64), index=True)
+    last_name = db.Column(db.String(64), index=True)
     username = db.Column(db.String(64), unique=True, index=True)
     orders = db.relationship('Association', back_populates='customer', lazy='dynamic')
+    password_hash = db.Column(db.String(128))
 
+    @property
+    def password(self):
+        raise AttributeError("Password is not a readable attribute")
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
+@login_manager.user_loader
+def load_user(customer_id):
+    return Customer.query.get(int(customer_id))
