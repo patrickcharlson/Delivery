ca1250fec81a7681b0a9fb5190865546bb89f590
charlson <patrickcharlson@gmail.com>
2019-03-28 16:43:11 +0300

    Added a blueprint

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,15 @@
 from flask import render_template
+from flask_login import login_required
+
 from . import main
 
 
-@main.route('/', methods=['GET', 'POST'])
+@main.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('main/index.html')
+
+
+@main.route('/welcome_page')
+@login_required
+def welcome_page():
+    return render_template('main/welcome_page.html')
